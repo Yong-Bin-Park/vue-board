bb5732f9e8becc37f0cef6c4f7d51f66163ed62c
Yong-Bin-Park <dydqls7592@gmail.com>
2022-12-15 08:22:23 +0900

    loginAPI

--- vue-frontboard/src/service/LoginAPI.js
+++ vue-frontboard/src/service/LoginAPI.js
@@ -1,33 +1,35 @@
 // src/service/loginAPI.js
+import axios from 'axios'
+
 const getUserInfo = (userId, userPw) => {
-    const reqData = {
-      'user_id': userId,
-      'user_pw': userPw
+  const reqData = {
+    'user_id': userId,
+    'user_pw': userPw
+  }
+
+  let serverUrl = '//localhost:8081'
+
+  return axios.post(serverUrl + '/user/login', reqData, {
+    headers: {
+      'Content-type': 'application/json'
     }
-  
-    return {
-      'data': {
-        'user_id': reqData.user_id,
-        'user_token': 'user_test_token',
-        'user_role': 'ADM'
+  })
+}
+
+export default {
+  async doLogin(userId, userPw) {
+    try {
+      const getUserInfoPromise = getUserInfo(userId, userPw)
+      const [userInfoResponse] = await Promise.all([getUserInfoPromise])
+      if (userInfoResponse.data.length === 0) {
+        return 'notFound'
+      } else {
+        localStorage.setItem('user_token', userInfoResponse.data.user_token)
+        localStorage.setItem('user_role', userInfoResponse.data.user_role)
+        return userInfoResponse
       }
+    } catch (err) {
+      console.error(err)
     }
   }
-  
-  export default {
-    async doLogin(userId, userPw) {
-      try {
-        const getUserInfoPromise = getUserInfo(userId, userPw)
-        const [userInfoResponse] = await Promise.all([getUserInfoPromise])
-        if (userInfoResponse.data.length === 0) {
-          return 'notFound'
-        } else {
-          localStorage.setItem('user_token', userInfoResponse.data.user_token)
-          localStorage.setItem('user_role', userInfoResponse.data.user_role)
-          return userInfoResponse
-        }
-      } catch (err) {
-        console.error(err)
-      }
-    }
-  }
+}
