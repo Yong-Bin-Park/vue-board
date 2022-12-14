f9464a1e0f05012467c00e0f390dd975d0ac33bc
Yong-Bin-Park <dydqls7592@gmail.com>
2022-12-14 10:42:07 +0900

    vue-front

---
+++ vue-frontboard/src/components/PageHeader.vue
@@ -0,0 +1,23 @@
+<!-- PageHeader.vue -->
+<template>
+    <div>
+        <header>
+        <div id="nav">
+            <router-link to="/">Home</router-link> |
+            <router-link to="/about">About</router-link> |
+            <router-link to="/board/list">게시판</router-link>
+        </div>
+        </header>
+        <hr/>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+  
+  }
+  </script>
+  
+  <style scoped>
+  
+  </style>
